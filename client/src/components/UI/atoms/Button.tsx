52c79e252f9d02cc8916cbb320c297b8a85e57ac
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-12-31 12:02:52 +0100

    Added disabled option to button component

--- client/src/components/UI/atoms/Button.tsx
+++ client/src/components/UI/atoms/Button.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components'
 import { colors, radius } from '../../../styles'
 
-const Button = styled.button`
-  background-color: ${colors.main};
-  border: 1px solid ${colors.main};
-  color: ${colors.white};
+interface ButtonProps {
+  disabled: boolean
+}
+
+const Button = styled.button.attrs((props: ButtonProps) => ({
+  backgroundColor: props.disabled ? colors.white : colors.main,
+  color: props.disabled ? colors.main : colors.white,
+  cursor: props.disabled ? 'default' : 'pointer'
+}))`
+  background-color: ${(props) => props.backgroundColor};
+  border: 1px solid ${(props) => props.color};
+  color: ${(props) => props.color};
   border-radius: ${radius};
-  cursor: pointer;
+  cursor: ${(props) => props.cursor};
   padding: 0.5rem 2rem;
 `
 
