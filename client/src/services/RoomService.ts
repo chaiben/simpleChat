da9366c75aaea296a3aca0fda774ceb5c59e49ba
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2023-01-31 07:45:44 +0100

    Changin room logic - WIP

--- client/src/services/RoomService.ts
+++ client/src/services/RoomService.ts
@@ -1,12 +1,12 @@
-import { ApiResponse } from '../interfaces/apiInterface'
+// import { ApiResponse } from '../interfaces/apiInterface'
 import { Room } from '../interfaces/roomInterface'
 import APIService from './APIService'
 
 export default class RoomService extends APIService {
-  // Method to create a new room
-  async create(room: Room): Promise<ApiResponse> {
-    return await this.post('rooms/create/', room)
-  }
+  // // Method to create a new room
+  // async create(room: Room): Promise<ApiResponse> {
+  //   return await this.post('rooms/create/', room)
+  // }
 
   // Method to receive all created rooms
   async getAll(): Promise<Room[]> {
